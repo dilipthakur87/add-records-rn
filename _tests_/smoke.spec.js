333aada39f251aadb18714b37432111143ae42d5
Dilip Thakur <dileepthakur87@gmail.com>
2020-06-01 15:07:58 +0545

    removed duplicate files

--- _tests_/smoke.spec.js
+++ _tests_/smoke.spec.js
@@ -1,5 +1,16 @@
-describe('truth', () => {
-    it('is ture', () => {
-        expect(true).toEqual(true);
-    });
-})
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import UserInputModal from '../components/UserInputModal'
+
+it('renders correctly', () => {
+    const tree = renderer.create(
+        <UserInputModal />
+    ).toJSON();   
+    expect(tree).toMatchSnapshot();
+});
+// describe('truth', () => {
+//     it('is ture', () => {
+//         expect(true).toEqual(true);
+//     });
+// })
